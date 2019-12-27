742bbf02361091376345e0cd1c18c2ec2d9347ee
ToniCifre <tonicifrev@gmail.com>
2019-12-27 20:42:40 +0100

    server print list of clients

--- server2.erl
+++ server2.erl
@@ -1,45 +1,52 @@
 -module(server2).
 
 %% Exported Functions
--export([start/0, start/1, init_server/0, init_server/1, process_requests/2]).
+-export([start/0, start/1, init_server/0, init_server/1, process_requests/3]).
 
 %% API Functions
 start() ->
 	ServerPid = spawn(server2, init_server, []),
-	register(myserver, ServerPid).
+	register(myserver, ServerPid),
+	process_commands(ServerPid).
 
 start(BootServer) ->
 	ServerPid = spawn(server2, init_server, [BootServer]),
 	register(myserver, ServerPid).
 
 init_server() ->
-	process_requests([], [self()]).
+	process_requests([], [self()], []).
 
 init_server(BootServer) ->
 	BootServer ! {server_join_req, self()},
-	process_requests([], []).
+	process_requests([], [], []).
 
-process_requests(Clients, Servers) ->
+process_requests(Clients, Servers, NameClients) ->
 	receive
 		%% Messages between client and server
 		% A new client joins
 		{client_join_req, Name, From} ->
 			io:format("[JOIN] ~s~n", [Name]),
 			NewClients = [From|Clients],
+			NewNameClients = [Name|NameClients],
 			broadcast(Servers, {join, Name}),
-			process_requests(NewClients, Servers);
+			process_requests(NewClients, Servers, NewNameClients);
 		% A client leaves
 		{client_leave_req, Name, From} ->
 			io:format("[EXIT] ~s~n", [Name]),
 			NewClients = lists:delete(From, Clients),
+			NewNameClients = lists:delete(Name, NameClients),
 			broadcast(Servers, {leave, Name}),
 			From ! exit,
-			process_requests(NewClients, Servers);
+			process_requests(NewClients, Servers, NewNameClients);
 		% A client sends a message
 		{send, Name, Text} ->
 			io:format("~p: ~s", [Name, Text]),
 			broadcast(Servers, {message, Name, Text}),
-			process_requests(Clients, Servers);
+			process_requests(Clients, Servers, NameClients);
+
+		print_users ->
+			even_print(NameClients),
+			process_requests(Clients, Servers, NameClients);
 
 		%% Messages between servers
 		% Stop the server
@@ -53,16 +60,16 @@ process_requests(Clients, Servers) ->
 			io:format("New server joins!~n", []),
 			NewServers = [From|Servers],
 			broadcast(NewServers, {update_servers, NewServers}),
-			process_requests(Clients, NewServers);
+			process_requests(Clients, NewServers, NameClients);
 		% Server list update
 		{update_servers, NewServers} ->
 			io:format("Server list update! ~w~n", [NewServers]),
-			process_requests(Clients, NewServers);
+			process_requests(Clients, NewServers, NameClients);
 		% Other messages are relayed to clients
 		RelayMessage ->
 			io:format("Relaying message...~n", []),
 			broadcast(Clients, RelayMessage),
-			process_requests(Clients, Servers)
+			process_requests(Clients, Servers, NameClients)
 
 	end.
 
@@ -72,3 +79,23 @@ broadcast(PeerList, Message) ->
 	lists:map(Fun, PeerList).
 
 
+%% This is the main task logic
+process_commands(ServerPid) ->
+	%% Read from the standard input and send to server
+	Text = io:get_line("-> "),
+    if Text == "List_clients\n" ->
+        ServerPid ! print_users;
+       Text == "exit\n" ->
+       	io:format("remove~n");
+       true ->
+        io:format("divisible by 5~n")
+    end,
+	process_commands(ServerPid).
+
+
+even_print([])-> [];
+even_print([H|T]) when H rem 2 /= 0 ->
+    even_print(T);
+even_print([H|T]) ->
+    io:format("~s~n", [H]),
+    [H|even_print(T)].
