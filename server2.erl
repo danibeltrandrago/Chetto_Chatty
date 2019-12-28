d1e8202c83359a63fa3e9b6e13ca5a5e31b17fc1
ToniCifre <tonicifrev@gmail.com>
2019-12-28 15:49:46 +0100

    server set list of pair: direction client and name client

--- server2.erl
+++ server2.erl
@@ -1,7 +1,7 @@
 -module(server2).
 
 %% Exported Functions
--export([start/0, start/1, init_server/0, init_server/1, process_requests/3]).
+-export([start/0, start/1, init_server/0, init_server/1, process_requests/2]).
 
 %% API Functions
 start() ->
@@ -14,72 +14,78 @@ start(BootServer) ->
 	register(myserver, ServerPid).
 
 init_server() ->
-	process_requests([], [self()], []).
+	process_requests([], [self()]).
 
 init_server(BootServer) ->
 	BootServer ! {server_join_req, self()},
-	process_requests([], [], []).
+	process_requests([], []).
 
-process_requests(Clients, Servers, NameClients) ->
+process_requests(Clients, Servers) ->
 	receive
 		%% Messages between client and server
 		% A new client joins
 		{client_join_req, Name, From} ->
 			io:format("[JOIN] ~s~n", [Name]),
-			NewClients = [From|Clients],
-			NewNameClients = [Name|NameClients],
-			broadcast(Servers, {join, Name}),
-			process_requests(NewClients, Servers, NewNameClients);
+			NewClients = [{From, Name}|Clients],
+			broadcast_server(Servers, {join, Name}),
+			process_requests(NewClients, Servers);
 		% A client leaves
 		{client_leave_req, Name, From} ->
 			io:format("[EXIT] ~s~n", [Name]),
-			NewClients = lists:delete(From, Clients),
-			NewNameClients = lists:delete(Name, NameClients),
-			broadcast(Servers, {leave, Name}),
+			NewClients = lists:delete({From, Name}, Clients),
+			broadcast_server(Servers, {leave, Name}),
 			From ! exit,
-			process_requests(NewClients, Servers, NewNameClients);
+			process_requests(NewClients, Servers);
 		% A client sends a message
 		{send, Name, Text} ->
 			io:format("~p: ~s", [Name, Text]),
-			broadcast(Servers, {message, Name, Text}),
-			process_requests(Clients, Servers, NameClients);
+			broadcast_server(Servers, {message, Name, Text}),
+			process_requests(Clients, Servers);
 
 		print_users ->
-			even_print(NameClients),
-			process_requests(Clients, Servers, NameClients);
+			print_name(Clients),
+			process_requests(Clients, Servers);
 
 		remove_users ->
 			remove_user(Clients),
 			io:format("All Clients removed~n"),
-			process_requests([], Servers, []);
+			process_requests([], Servers);
 
 		%% Messages between servers
 		% Stop the server
 		disconnect ->
 			io:format("Bye!~n"),
 			NewServers = lists:delete(self(), Servers),
-			broadcast(NewServers, {update_servers, NewServers}),
+			broadcast_server(NewServers, {update_servers, NewServers}),
 			unregister(myserver);
 		% Server joins
 		{server_join_req, From} ->
 			io:format("New server joins!~n"),
 			NewServers = [From|Servers],
 			broadcast(NewServers, {update_servers, NewServers}),
-			process_requests(Clients, NewServers, NameClients);
+			process_requests(Clients, NewServers);
 		% Server list update
 		{update_servers, NewServers} ->
 			io:format("Server list update! ~w~n", [NewServers]),
-			process_requests(Clients, NewServers, NameClients);
+			process_requests(Clients, NewServers);
 		% Other messages are relayed to clients
 		RelayMessage ->
 			io:format("Relaying message...~n"),
 			broadcast(Clients, RelayMessage),
-			process_requests(Clients, Servers, NameClients)
+			process_requests(Clients, Servers)
 
 	end.
 
 %% Local Functions
 broadcast(PeerList, Message) ->
+	Fun = fun(Peer) -> 
+		X = element(1, Peer),
+		X ! Message 
+	end,
+	lists:map(Fun, PeerList).
+
+%% Local Functions
+broadcast_server(PeerList, Message) ->
 	Fun = fun(Peer) -> Peer ! Message end,
 	lists:map(Fun, PeerList).
 
@@ -103,18 +109,18 @@ process_commands(ServerPid) ->
         io:format("Este comando no existe, Introduce 'help' para ver las opciones.~n")
     end,
 	process_commands(ServerPid).
+	
 
-
-even_print([])-> [];
-even_print([H|T]) when H rem 2 /= 0 ->
-    even_print(T);
-even_print([H|T]) ->
-    io:format("~s~n", [H]),
-    [H|even_print(T)].
+print_name(Clients) ->
+	P = fun(Client) ->
+			Name = element(2, Client),
+			io:format("~s~n", [Name])
+		end,
+	lists:map(P, Clients).
 
 remove_user(Clients) ->
 	P = fun(Client) ->
-			io:format("[Removed]~n"),
-			Client ! exit
+			Direction = element(1, Client),
+			Direction ! exit
 		end,
 	lists:map(P, Clients).
